8801499fc884003ff6cec5dedb83cd9990677b92
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2023-05-13 15:23:52 +0400

    Created connention with mongoDb, taking add new coffee data from client side and sending those data to DB.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,9 +10,48 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cp5mulo.mongodb.net/?retryWrites=true&w=majority`;
+
 app.get("/", (req, res) => {
     res.send("Coffee Espresso Emporium Server is Running")
 })
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const coffeeCollection = client.db("coffeeDB").collection("coffee");
+    // create a document to insert
+    
+    app.post("/coffee", async (req, res) => {
+      const newCoffee = req.body;
+      console.log("new Coffee Added", newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.listen(port, () => {
     console.log(`Our espresso emporium server is running on ${port}`);
